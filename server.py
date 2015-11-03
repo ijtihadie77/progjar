c51e1ce438e436eec46edffc6f9b7f2a215ba1e5
Royyana <royyana@gmail.com>
2015-11-03 13:16:11 +0700

    webserver1 mengambil seluruh data yang diterima

--- server.py
+++ server.py
@@ -1,31 +1,47 @@
-import sys
+import threading
 import socket
+import time
+import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class MemprosesClient(threading.Thread):
+	def __init__(self,client_socket,client_address,nama):
+		self.client_socket = client_socket
+		self.client_address = client_address
+		self.nama = nama
+		threading.Thread.__init__(self)
+	
+	def run(self):
+		message = ''
+		while True:
+        		data = self.client_socket.recv(32)
+            		if data:
+				message = message + data #collect seluruh data yang diterima
+            		else:
+               			break
+		self.client_socket.close()
+		
 
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
 
-# Listen for incoming connections
-sock.listen(1)
+class Server(threading.Thread):
+	def __init__(self):
+		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server_address = ('localhost',9999)
+		self.my_socket.bind(self.server_address)
+		threading.Thread.__init__(self)
 
-while True:
-    	# Wait for a connection
-    	print >>sys.stderr, 'waiting for a connection'
-    	connection, client_address = sock.accept()
-    	print >>sys.stderr, 'connection from', client_address
-    	# Receive the data in small chunks and retransmit it
-    	while True:
-        	data = connection.recv(32)
-        	print >>sys.stderr, 'received "%s"' % data
-            	if data:
-                	print >>sys.stderr, 'sending data back to the client'
-                	connection.sendall(data)
-            	else:
-                	print >>sys.stderr, 'no more data from', client_address
-                	break
-        # Clean up the connection
-	connection.close()
+	def run(self):
+		self.my_socket.listen(1)
+		nomor=0
+		while (True):
+			self.client_socket, self.client_address = self.my_socket.accept()
+    			nomor=nomor+1
+			#---- menghandle message cari client (Memproses client)
+			my_client = MemprosesClient(self.client_socket, self.client_address, 'PROSES NOMOR '+str(nomor))
+			my_client.start()
+			#----
+
+
+serverku = Server()
+serverku.start()
+
+
